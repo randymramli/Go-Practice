d2f10a8efe0dca279ade513d5a6094ef93d5967c
Randy Ramli <ramli.r@northeastern.edu>
2025-05-15 16:18:53 +0700

    update

--- bill.go
+++ bill.go
@@ -1,5 +1,9 @@
 package main
 
+import (
+	"fmt"
+)
+
 type bill struct {
 	name  string
 	items map[string]float64
@@ -14,7 +18,31 @@ func newBill(name string) bill {
 		tip:   0,
 		total: 0,
 	}
-
 	return b
+}
 
+func (b bill) format() string {
+	fs := "Bill breakdown: \n"
+	// var total float64 = 0
+
+	for k, v := range b.items {
+		fs += fmt.Sprintf("%-25v ...%v \n", k+":", v)
+		b.total += v
+	}
+
+	b.total += b.tip
+
+	fs += fmt.Sprintf("%-25v ...%.2f\n", "Tip:", b.tip)
+
+	fs += fmt.Sprintf("%-25v ...%.2f", "Total:", b.total)
+
+	return fs
+}
+
+func (b *bill) addTip(tip float64) {
+	b.tip = tip
+}
+
+func (b *bill) addItem(name string, price float64) {
+	b.items[name] = price
 }
--- main.go
+++ main.go
@@ -5,8 +5,12 @@ import "fmt"
 func main() {
 
 	myBill := newBill("Mario's Bill")
-
+	myBill.addItem("Shrimp Salad", 14.99)
+	myBill.addItem("Pie", 5.99)
+	myBill.addItem("Coke Float", 4.99)
+	myBill.addTip(3.50)
 	fmt.Println(myBill)
+	fmt.Println(myBill.format())
 
 }
 
